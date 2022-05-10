4ba75f94d783e381ef8d2ce9ee3290c3953dfb49
everalert <everalert@gmail.com>
2022-05-11 01:09:30 +1000

    adjust level XP corner size

--- src/modules/scoreboard.module.js
+++ src/modules/scoreboard.module.js
@@ -10,7 +10,7 @@ export default function Scoreboard() {
 					<div className="iconText">77</div>
 					<div className="iconImage">SEASON SVG ICON</div>
 					<div className="iconProgressBar"><RadialBar size={308} value={85000} thickness={40} /></div>
-					<div className="iconSubProgressBar"><RadialBar size={256} value={900} max={1000} thickness={4} corner={0} delay={250} /></div>
+					<div className="iconSubProgressBar"><RadialBar size={256} value={900} max={1000} thickness={4} corner={1} delay={250} /></div>
 				</div>
 				<h1>ALIGNMENT</h1>
 				<h2>55 DAYS REMAIN</h2>
