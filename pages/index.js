96d90e6d181c5b2b02c3fba95782be58e0189023
everalert <everalert@gmail.com>
2022-05-12 18:51:05 +1000

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import styles from '../src/css/Home.module.css'
 import Sidebar from '../src/modules/Sidebar.module'
 import Scoreboard from '../src/modules/Scoreboard.module'
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Scoreboard/>
 			<Sidebar/>
 		</div>
