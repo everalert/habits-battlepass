4a9ad42321aebbdf8003a446d56a0e6b6282cefe
everalert <everalert@gmail.com>
2022-05-13 23:52:27 +1000

    fix GoalPanel logo circles

--- src/modules/goalpanel.module.js
+++ src/modules/goalpanel.module.js
@@ -19,8 +19,8 @@ export default function GoalPanel() {
 			</div>
 			<div className='flex gap-4 justify-center'>
 				<div className='relative'>
-					<svg xmlns="http://www.w3.org/2000/svg" class="absolute h-28 w-28 left-2 top-2" viewBox="0 0 20 20" fill="currentColor">
-						<path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+					<svg xmlns="http://www.w3.org/2000/svg" class="absolute h-32 w-32 left-0 top-0" viewBox="0 0 20 20" fill="currentColor">
+						<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 0 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
 					</svg>
 					<div className="w-32 h-32">
 						<RadialBar size={128} value={15000} max={25000} thickness={8} corner={1} delay={500} />
