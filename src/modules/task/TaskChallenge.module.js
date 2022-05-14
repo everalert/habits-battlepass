3ea1715c07b408376123726bb7f61bc171f41464
everalert <everalert@gmail.com>
2022-05-15 03:23:54 +1000

    rework progress bar in TaskChallenge.module

--- src/modules/task/TaskChallenge.module.js
+++ src/modules/task/TaskChallenge.module.js
@@ -1,4 +1,5 @@
 export default function TaskChallenge(props) {
+	const progress = props.over/props.under*100;
 	return (
 		<div className="clear-both flex flex-row-reverse gap-x-2">
 			<div className="text-lg font-bold text-center tracking-tighter block w-16 h-8 bg-gray-500 shrink-0">{props.reward}</div>
@@ -9,7 +10,11 @@ export default function TaskChallenge(props) {
 					<span>/</span>
 					<span>{props.under}</span>
 				</span>
-				<span className="clear-left block bg-gray-800 w-full h-2"></span>
+				<div className="bg-zinc-900 clear-left w-full mt-[1.625rem] h-1.5 -skew-x-[24deg] overflow-hidden">
+					<div
+						className='bg-zinc-500 w-full h-full absolute'
+						style={{left:`${-100+progress}%`}}></div>
+				</div>
 			</div>
 		</div>
 	);
--- src/modules/task/TaskCollection.module.js
+++ src/modules/task/TaskCollection.module.js
@@ -2,7 +2,7 @@ import TaskChallenge from "./TaskChallenge.module";
 
 export default function TaskCollection(props) {
 	return (
-		<div className="w-11/12 mx-auto flex flex-col gap-2">
+		<div className="w-[19rem] mx-auto flex flex-col gap-2">
 			<h3 className='text-lg font-bold tracking-tighter'>{props.label}</h3>
 			<TaskChallenge label='Watch 2 episodes of anime' over={1} under={2} reward={1000} />
 			<TaskChallenge label='Read 4 chapters of manga' over={3} under={4} reward={1000} />
