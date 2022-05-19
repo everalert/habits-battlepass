7e872797b2a9cb8d7f06f0a4ca1386e11677d864
everalert <everalert@gmail.com>
2022-05-20 06:57:59 +1000

    add SecondsToMinutes, SecondsToTimeObject

--- src/helpers/Math.helper.js
+++ src/helpers/Math.helper.js
@@ -2,9 +2,22 @@ export function GetCurrentUnixTimestamp() {
 	return Math.floor(new Date().getTime()/1000)
 }
 
+export function SecondsToTimeObject(seconds) {
+	return {
+		d: Math.floor(seconds/86400),
+		h: Math.floor(seconds%86400/3600),
+		m: Math.floor(seconds%3600/60), 
+		s: Math.floor(seconds%60),
+		ms: seconds%1
+	}
+}
+
 export function SecondsToHMMSS(seconds) {
-	const h = Math.floor(seconds/3600)
-	const m = Math.floor(seconds%3600/60)
-	const s = seconds%3600%60
-	return `${String(h)}:${String(m).padStart(2,'0')}:${String(s).padStart(2,'0')}`
+	const {d, h, m, s} = SecondsToTimeObject(seconds)
+	return `${String(d*24+h)}:${String(m).padStart(2,'0')}:${String(s).padStart(2,'0')}`
+}
+
+export function SecondsToMinutes(seconds) {
+	const {d, h, m, s, ms} = SecondsToTimeObject(seconds)
+	return d*1440+h*60+m+(s+ms)/60
 }
