8dec9f8f182ca07a71bc72040ed59c2e795f0870
everalert <everalert@gmail.com>
2022-05-20 02:10:25 +1000

    cleanup Challenge.helper

--- src/redux/helpers/Challenge.helper.js
+++ src/redux/helpers/Challenge.helper.js
@@ -1,12 +1,10 @@
-import { enableMapSet } from "immer";
 import { useSelector } from "react-redux";
-import { GetCurrentUnixTimestamp } from "../../helpers/Math.helper";
 
 export function GetAllChallengesForGoal(goalId) {
 	return useSelector((state) => state.challenge.challenges.filter(c => c.goalId === goalId))
 }
 
-export function GetAllChallengesForGoalOfPeriod(goalId,period) {
+export function GetAllChallengesForGoalOfPeriod(goalId, period) {
 	return useSelector((state) => state.challenge.challenges.filter(c => c.goalId === goalId && c.period === period))
 }
 
