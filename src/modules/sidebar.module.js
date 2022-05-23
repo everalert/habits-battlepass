cc018f26c46a601c1cdcf118dbdc898fb9a5822e
everalert <everalert@gmail.com>
2022-05-23 12:47:57 +1000

    prep sidebar with test button

--- src/modules/sidebar.module.js
+++ src/modules/sidebar.module.js
@@ -1,4 +1,4 @@
-import { DocumentAddIcon, DocumentDownloadIcon, HomeIcon, MenuIcon } from '@heroicons/react/solid';
+import { ClipboardCheckIcon, DocumentAddIcon, DocumentDownloadIcon, HomeIcon, MenuIcon } from '@heroicons/react/solid';
 import { connect, useDispatch } from 'react-redux';
 import { ExportDataToODS, ImportDataFromODS } from '../helpers/ODS.helper';
 import { replaceRecords } from '../redux/slices/Manager.slice';
@@ -26,6 +26,7 @@ const mapStateToProps = (state) => {
 
 const Sidebar = ({data, bases}) => {
 	const dispatch = useDispatch();
+
 	const onDragOver = (e) => {
 		e.preventDefault();
 	}
@@ -38,10 +39,13 @@ const Sidebar = ({data, bases}) => {
 	}
 
 	return (
-		<nav className='fixed inset-y-0 -left-0 hover:left-0 w-12 p-1 transition-all group'>
+		<nav className='fixed inset-y-0 -left-0 hover:left-0 flex flex-col gap-1 w-12 p-1 transition-all group'>
 			<MenuIcon className='w-8 h-8 m-1 hover:cursor-pointer relative left-0 group-hover:left-0 transition-all'/>
 			<div className='p-1 flex flex-col gap-1'>
 				<HomeIcon className='w-8 h-8 hover:cursor-pointer'/>
+			</div>
+			<div className='bg-green-900/25 rounded-lg p-1 flex flex-col gap-1'>
+				<ClipboardCheckIcon className='w-8 h-8 hover:cursor-pointer' onClick={()=>{}} />
 			</div>
 			<div className='bg-red-900/25 rounded-lg p-1 flex flex-col gap-1'>
 				<DocumentAddIcon className='w-8 h-8' onDrop={onDrop} onDragOver={onDragOver}/>
