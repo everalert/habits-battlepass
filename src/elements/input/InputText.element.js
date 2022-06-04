4da197d5e57bf83a8d287842ab7d2518b943b9d0
everalert <everalert@gmail.com>
2022-06-05 02:50:38 +1000

    fix InputText uncontrolled input error

--- src/elements/input/InputText.element.js
+++ src/elements/input/InputText.element.js
@@ -2,7 +2,7 @@ export default function InputText({ text, setParentText, short, onFocus, onBlur 
 
 	return (
 		<label className={`${short && 'w-20'} h-7 flex items-center px-2 rounded text-zinc-600 caret-zinc-500 bg-zinc-300`}>
-			<input type="text" placeholder="text" value={text} onChange={(e)=>setParentText(e.target.value)} onFocus={onFocus} onBlur={onBlur} className="w-full bg-transparent focus:outline-none focus:text-zinc-900 placeholder:text-zinc-400 text-left" />
+			<input type="text" placeholder="text" value={text ?? ''} onChange={(e)=>setParentText(e.target.value)} onFocus={onFocus} onBlur={onBlur} className="w-full bg-transparent focus:outline-none focus:text-zinc-900 placeholder:text-zinc-400 text-left" />
 		</label>
 	)
 
