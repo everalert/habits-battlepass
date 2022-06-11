2a37c33fd172a92c2ca9fd1603ff5d33b0f8bcb6
everalert <everalert@gmail.com>
2022-06-12 09:06:51 +1000

    add asyncDispatch middleware

--- src/redux/ReduxStore.js
+++ src/redux/ReduxStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import SeasonReducer from './slices/Season.slice'
-import CategoryReducer from './slices/Category.slice'
-import GoalReducer from './slices/Goal.slice'
-import ChallengeReducer from './slices/Challenge.slice'
-import ActivityReducer from './slices/Activity.slice'
-import LogReducer from './slices/Log.slice'
+import asyncDispatch from "./middleware/asyncDispatch.middleware";
+import ActivityReducer from './slices/Activity.slice';
+import CategoryReducer from './slices/Category.slice';
+import ChallengeReducer from './slices/Challenge.slice';
+import GoalReducer from './slices/Goal.slice';
+import LogReducer from './slices/Log.slice';
+import SeasonReducer from './slices/Season.slice';
+
 
 export const store = configureStore({
 	reducer: {
@@ -15,4 +17,5 @@ export const store = configureStore({
 		activity: ActivityReducer,
 		log: LogReducer
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(asyncDispatch)
 });
---
+++ src/redux/middleware/asyncDispatch.middleware.js
@@ -0,0 +1,27 @@
+//https://stackoverflow.com/a/41260990
+
+export default store => next => action => {
+	let syncActivityFinished = false;
+	let actionQueue = [];
+
+	function flushQueue() {
+		actionQueue.forEach(a => store.dispatch(a)); // flush queue
+		actionQueue = [];
+	}
+
+	function asyncDispatch(asyncAction) {
+		actionQueue = actionQueue.concat([asyncAction]);
+
+		if (syncActivityFinished) {
+			flushQueue();
+		}
+	}
+
+	const actionWithAsyncDispatch =
+		Object.assign({}, action, { asyncDispatch });
+
+	const r = next(actionWithAsyncDispatch);
+	syncActivityFinished = true;
+	flushQueue();
+	return r;
+};
