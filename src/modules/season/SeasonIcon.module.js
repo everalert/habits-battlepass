85558a54792d3d641eea4ee6bb16b84bcc0f97b1
everalert <everalert@gmail.com>
2022-06-22 09:16:07 +1000

    add SeasonIcon component

---
+++ src/modules/season/SeasonIcon.module.js
@@ -0,0 +1,7 @@
+export default function SeasonIcon() {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" className="w-64 h-64 absolute top-4 left-4" viewBox="0 0 20 20" fill="currentColor">
+			<circle cx='10' cy='10' r='8' />
+		</svg>
+	)
+}
